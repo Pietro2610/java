3bf3ed9cbe4948f5c9f50963b01a799db8bd0a72
Piotr <harmit@gmail.com>
2025-03-07 14:08:56 +0100

    praca domowa 3

---
+++ src/PracaDomowa3.java
@@ -0,0 +1,22 @@
+public class PracaDomowa3 {
+    public static void main(String[] args) {
+/*
+        for (int i=0; i<=100; i++) {
+            if (i%3 == 0) {
+                System.out.println(i);
+            }
+        }
+*/
+        int[] numbers = new int[] {1,3,5,7,0};
+
+        for (int i=0;i<(numbers.length/2); i++) {
+            int temp = numbers[i]; //tymczasowa zmienna int dla pierwszego wyrażenia w tablicy
+            numbers[i] = numbers[numbers.length-1-i];
+            numbers[numbers.length-1-i] = temp;
+        }
+
+        for (int i=0; i<numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
