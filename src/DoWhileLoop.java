15a19638961d252fe3961838ff76f0d25f710daf
Piotr <harmit@gmail.com>
2025-03-07 12:59:52 +0100

    petla do while

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int number = 16;
+        do {
+            System.out.println("Moja wartość to " + number + " i jestem mniejsza od 20");
+            number++;
+        }
+        while (number < 20);
+    }
+}
