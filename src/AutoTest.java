d9de166276d3281d518527e2bb6d25c713accc6d
Piotr <harmit@gmail.com>
2025-04-26 21:29:06 +0200

    praca domowa konstruktory

--- src/AutoTest.java
+++ src/AutoTest.java
@@ -1,26 +1,28 @@
 public class AutoTest {
     public static void main(String[] args) {
 
-      Auto mercedes = new Auto();
+        Auto auto = new Auto("Audi", "A6", 2003, 1200);
+        auto.info();
+/*      Auto mercedes = new Auto();
         mercedes.marka = "Mercedes";
         mercedes.model = "Klasa S";
         mercedes.rok = 1999;
-        mercedes.przebieg = 1000;
+        mercedes.przebieg = 1000;*/
 
    /*     mercedes.jedz();
         mercedes.hamuj();
         mercedes.info();*/
 
-      Auto audi = new Auto();
+/*      Auto audi = new Auto();
       audi.marka = "Audi";
       audi.model = "A5";
       audi.rok = 2020;
-      audi.przebieg = 0;
+      audi.przebieg = 0;*/
     //  audi.info();
 
-      Auto noname = new Auto();
+/*      Auto noname = new Auto();
 
-      noname.info();
+      noname.info();*/
 
     }
 }
