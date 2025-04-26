657deaab6a2100c937758558cd0aa6413fc3df3c
Piotr <harmit@gmail.com>
2025-04-26 14:50:33 +0200

    metody

--- src/Metody.java
+++ src/Metody.java
@@ -1,11 +1,27 @@
 public class Metody {
-    public void nazwaMetody() {
+    /*    public int nazwaMetody() {
+            System.out.println("zaraz policzę wynik");
+            int result =0;
+            for (int i=0; i<=100; i++) {
+                result = result + i;
+            }
+            System.out.println("wynik to: "+ result);
+
+            return result;
+        }*/
+/*    public void policzWynik(int number) {
+        System.out.println("Number ma wartość " + number);
         System.out.println("zaraz policzę wynik");
-        int result =0;
-        for (int i=0; i<=100; i++) {
+        int result = 0;
+        for (int i = 0; i <= number; i++) {
             result = result + i;
         }
-        System.out.println("wynik to: "+ result);
+        System.out.println("wynik to: " + result);
+    }*/
+    public void dodawanie(int dodaj1, int dodaj2) {
+        System.out.println("dodaj1 ma wartość "+ dodaj1);
+        System.out.println("dodaj2 ma wartość "+ dodaj2);
+        int wynikDodawania = dodaj1 + dodaj2;
+        System.out.println("Wynik dodawania to "+ wynikDodawania);
     }
-
 }
--- src/MetodyTest.java
+++ src/MetodyTest.java
@@ -1,6 +1,11 @@
 public class MetodyTest {
-    public static void main(String[] args) {
-        Metody metody = new Metody();
-        metody.nazwaMetody();
-    }
+   public static void main(String[] args) {
+       Metody metody = new Metody();
+/*        int result = metody.nazwaMetody();
+        System.out.println(result);*/
+
+/*       metody.policzWynik(5);
+       metody.policzWynik(3);*/
+        metody.dodawanie(5, 3);
+   }
 }
