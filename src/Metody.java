2fd55ea3e373536b6fbf8726472264d05dcd0b41
Piotr <harmit@gmail.com>
2025-04-26 04:46:16 +0200

    metody

---
+++ src/Metody.java
@@ -0,0 +1,11 @@
+public class Metody {
+    public void nazwaMetody() {
+        System.out.println("zaraz policzę wynik");
+        int result =0;
+        for (int i=0; i<=100; i++) {
+            result = result + i;
+        }
+        System.out.println("wynik to: "+ result);
+    }
+
+}
---
+++ src/MetodyTest.java
@@ -0,0 +1,6 @@
+public class MetodyTest {
+    public static void main(String[] args) {
+        Metody metody = new Metody();
+        metody.nazwaMetody();
+    }
+}
