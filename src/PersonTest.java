f2e02e113fe9efb6b832e98df7f19f1e6be42af0
Piotr <harmit@gmail.com>
2025-05-02 04:03:26 +0200

    słowo super

--- src/PersonTest.java
+++ src/PersonTest.java
@@ -1,21 +1,21 @@
 public class PersonTest {
     public static void main(String[] args) {
 
-        MathTeacher teacher = new MathTeacher();
-        teacher.school = "AGH";
+        MathTeacher teacher = new MathTeacher("John", 29, "KFC");
+/*        teacher.school = "AGH";
         System.out.println(teacher.school);
         teacher.name = "David";
-        teacher.age = 35;
+        teacher.age = 35;*/
         teacher.walk();
         teacher.eat();
         teacher.teachMath();
         teacher.sayHello();
 
-        Footballer footballer = new Footballer();
-        footballer.footballClub = "Narnia United";
+        Footballer footballer = new Footballer("Tim", 27, "HWP");
+/*        footballer.footballClub = "Narnia United";
         System.out.println(footballer.footballClub);
         footballer.name = "Tim";
-        footballer.age = 23;
+        footballer.age = 23;*/
         footballer.walk();
         footballer.eat();
         footballer.playFootball();
